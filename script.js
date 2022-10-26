96a6cad4063710cf4f2936cd0ac6ec39ab88c62f
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-10-26 14:39:31 +0200

    esercitazione finita

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+const { createApp } = Vue;
+    
+
+
+
+createApp({
+  data() {
+    
+    return {
+      message: 'Hello World!',
+      image: './img/crema.jpg'
+    }
+  }
+}).mount('#app')
